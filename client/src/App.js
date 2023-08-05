3750689929cfada92431abb937ec6fc13828de20
dhanu1402 <dhanukumar2000@gmail.com>
2023-08-05 13:35:15 +0530

    join completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Chat from './components/Chat';
-import Join from './components/Join';
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
 
 export default function App() {
   return (
