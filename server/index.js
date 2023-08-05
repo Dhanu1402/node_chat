7fb013d6cbbf2353fc2fce97364da3433ced1a4f
dhanu1402 <dhanukumar2000@gmail.com>
2023-08-05 13:09:25 +0530

    socket conncetion made

--- server/index.js
+++ server/index.js
@@ -14,6 +14,14 @@ const io = socketio(server);
 
 app.use(router);
 
+io.on('connection', (socket) => {
+  console.log('New user up!');
+
+  socket.on('disconnect', () => {
+    console.log('User left!');
+  });
+});
+
 server.listen(3001, function () {
   console.log('listening on port 3001');
 });
